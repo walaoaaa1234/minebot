c01dda00e59a5fea3e8ae0e93eb134c5c7365f47
walaoaaa1234 <walaoaaa1234@hotmail.com>
2016-06-30 17:48:02 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ mkdir -p releases/$tag || exit 1
 
 for mod in Minebot AimBow; do
 	echo "Starting to compile $mod ..."
-	cd "$base/$mod"
+	cd "./$mod"
 	./gradlew build || exit 1
 	cp "build/libs/$(ls -t1 build/libs/ | tail -n 1)" "./releases/$tag" || exit 1
 done
